64796edcf134e37bd6481ac6fd8568a3fbe599f2
ORI_BER <ori.berman@amdocs.com>
2021-12-29 20:54:17 +0200

    inventory

--- catalog-service/src/main/java/com/amdocs/catalogservice/services/ProductService.java
+++ catalog-service/src/main/java/com/amdocs/catalogservice/services/ProductService.java
@@ -2,10 +2,16 @@ package com.amdocs.catalogservice.services;
 
 import com.amdocs.catalogservice.entities.Product;
 import com.amdocs.catalogservice.repositories.ProductRepository;
+import com.amdocs.catalogservice.web.models.ProductInventoryResponse;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,10 +20,12 @@ import java.util.Optional;
 @Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, RestTemplate restTemplate) {
         this.productRepository = productRepository;
+        this.restTemplate = restTemplate;
     }
 
     public List<Product> findAllProducts() {
@@ -25,6 +33,22 @@ public class ProductService {
     }
 
     public Optional<Product> findProductByCode(String code) {
-        return productRepository.findByCode(code);
+        Optional<Product> productOptional = productRepository.findByCode(code);
+        if (productOptional.isPresent()) {
+            log.info("Fetching inventory level for product_code: " + code);
+            ResponseEntity<ProductInventoryResponse> itemResponseEntity = restTemplate.getForEntity(
+                    "http://inventory-service/api/inventory/{code}",
+                    ProductInventoryResponse.class,
+                    code);
+            if (itemResponseEntity.getStatusCode() == HttpStatus.OK) {
+                Integer quantity = itemResponseEntity.getBody().getAvailableQuantity();
+                log.info("Available quantity: " + quantity);
+                productOptional.get().setInStock(quantity > 0);
+            } else {
+                log.error("Unable to get inventory level for product_code: " + code +
+                        ", StatusCode: " + itemResponseEntity.getStatusCode());
+            }
+        }
+        return productOptional;
     }
 }
