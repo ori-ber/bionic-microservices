f560f9f467103f0a5f1a464d7daffa1538d48182
ORI_BER <ori.berman@amdocs.com>
2022-01-02 13:55:06 +0200

    inventory +API

--- inventory-service/src/main/java/com/amdocs/inventoryservice/web/controllers/InventoryController.java
+++ inventory-service/src/main/java/com/amdocs/inventoryservice/web/controllers/InventoryController.java
@@ -34,18 +34,16 @@ public class InventoryController {
         }
     }
 
-    // @GetMapping("/api/inventory/code/{productCode}")
-    // public ResponseEntity<InventoryItem>
-    // findInventoryByProduct(@PathVariable("productCode") String productCode) {
-    // log.info("Finding inventory for product code :" + productCode);
-    // Optional<InventoryItem> inventoryItem =
-    // inventoryItemRepository.findByProductCode(productCode);
-    // if (inventoryItem.isPresent()) {
-    // return new ResponseEntity(inventoryItem, HttpStatus.OK);
-    // } else {
-    // return new ResponseEntity(HttpStatus.NOT_FOUND);
-    // }
-    // }
+    @GetMapping("/api/inventory/code/{productCode}")
+    public ResponseEntity<InventoryItem> findInventoryByProduct(@PathVariable("productCode") String productCode) {
+        log.info("Finding inventory for product code :" + productCode);
+        Optional<InventoryItem> inventoryItem = inventoryItemRepository.findByProductCode(productCode);
+        if (inventoryItem.isPresent()) {
+            return new ResponseEntity(inventoryItem, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @GetMapping("/api/inventory")
     public List<InventoryItem> getInventory() {
